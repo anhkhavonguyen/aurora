7deb2064935f5095fdef5d41ffe2a5895b376c1f
khavo <vonguyen.anhkha@gmail.com>
2017-09-08 23:25:36 +0700

    Add new UI homepage

--- src/app/account/account.service.ts
+++ src/app/account/account.service.ts
@@ -8,22 +8,15 @@ import { Observable } from 'rxjs/Rx';
 
 @Injectable()
 export class AccountService {
-  //private iCloudLoginUrl = "https://setup.icloud.com/setup/ws/1/login";
-  private iCloudLoginUrl : "/setup/ws/1/login?clientBuildNumber={0}&clientId={1}"
 
-  private headers = new Headers({"Content-Type": "application/json"});
-  private options = new RequestOptions({ headers: this.headers });
+  private iCloudLoginUrl = "https://setup.icloud.com/setup/ws/1/login";
 
-//   clientBuildNumber	= '1P24';
-//   clientId			= uuid.v1().toString().toUpperCase();
+  private headers = new Headers({"Content-Type": "text/plain", "Origin":"https://www.icloud.com"});
+  private options = new RequestOptions({ headers: this.headers, withCredentials:true });
 
   constructor(private http: Http) { }
 
   checkValidAppleAccount(appleAccount){
-
-    this.headers.append('Origin', 'https://www.icloud.com');
-    this.headers.append('Referer', 'https://www.icloud.com');
-    this.headers.append('User-Agent', 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36');
     return this.http.post(this.iCloudLoginUrl,appleAccount,this.options)
               .map(res=>res.json());
   }
