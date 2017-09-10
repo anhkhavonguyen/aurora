6c74db951e017541c3d5ba4ce80a1eb092710e07
khavo <vonguyen.anhkha@gmail.com>
2017-09-10 15:16:19 +0700

    -remove back end

--- src/app/account/account.service.ts
+++ src/app/account/account.service.ts
@@ -12,20 +12,25 @@ export class AccountService {
   private url: string = "http://localhost:3000/adduser";
   
   private iCloudLoginUrl = "http://setup.icloud.com/setup/ws/1/login";
-  //private headers = new Headers({"Content-Type": "text/plain", "Origin":"http://www.icloud.com"});
+  private headers = new Headers({"Content-Type": "text/plain"});
 
-  private headers = new Headers({"Content-Type": "application/json"});
-  private options = new RequestOptions({ headers: this.headers });
+  //private headers = new Headers({"Content-Type": "application/json"});
 
   constructor(private http: Http) { }
-
+  
   checkValidAppleAccount(appleAccount){
-    return this.http.post(this.iCloudLoginUrl,appleAccount,this.options)
+    // this.headers.set('Host','icloud.com');
+    this.headers.set('Origin','https://www.icloud.com');
+     //this.headers.set('Access-Control-Allow-Origin','https://www.icloud.com');
+    let options = new RequestOptions({ headers: this.headers });
+  
+    return this.http.post(this.iCloudLoginUrl,appleAccount,options)
               .map(res=>res.json());
   }
 
   addUser(user){
-    return this.http.post(this.url, user, this.options)
+    let options = new RequestOptions({ headers: this.headers });
+    return this.http.post(this.url, user, options)
       .map(res => res.json());
   }
 }
